49a2b720d7766b499ab2b2c0607d48964d3ae261
driesva <Dries.VanAutreve@telenet.be>
2019-04-09 10:57:51 +0200

    APOCOW-7 vl-ui-input-group - documentatie

--- vl-input-group.src.js
+++ vl-input-group.src.js
@@ -2,6 +2,13 @@ import { VlElement } from '/node_modules/vl-ui-core/vl-core.js';
 
 /**
  * vl-ui-input-group
+ * Gebruik vl-ui-input-group om een 'input field' en een 'input add-on' te combineren.
+ * Bijvoorbeeld: de 'vl-datepicker' component combineert een 'input field' en een 'input add-on' in een 'input group'.
+
+ * ### Slots
+ * Slot | Uitleg
+ * -----|--------
+ * `default` | De content van het default slot wordt toegevoegd als content van de groep.
  *
  * @demo demo/vl-ui-input-group.html
  */
