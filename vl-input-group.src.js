2c9b5f0105c721076b39b0731e8be43d566831d0
Tom Coemans <tom.coemans@gmail.com>
2019-05-03 08:23:29 +0200

    WEBC-80 WEBC-81 demo pagina upgrade en ecstatic versie werd verwijderd

--- vl-input-group.src.js
+++ vl-input-group.src.js
@@ -13,18 +13,16 @@ import { VlElement } from '/node_modules/vl-ui-core/vl-core.js';
  * @demo demo/vl-ui-input-group.html
  */
 export class VlInputGroup extends VlElement(HTMLElement) {
-
   constructor() {
     super(`
-            <style>
-                @import "../style.css";
-            </style>
-            <div class="vl-input-group">
-              <slot></slot>
-            </div>
-        `);
+      <style>
+        @import "../style.css";
+      </style>
+      <div class="vl-input-group">
+        <slot></slot>
+      </div>
+    `);
   }
-
 }
 
 customElements.define('vl-input-group', VlInputGroup);
