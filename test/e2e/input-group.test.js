68975b0c57123584f0b35ee1a0e2c1f668335abc
Tom Coemans <tom.coemans@gmail.com>
2020-06-03 14:01:18 +0200

    ESlint

--- test/e2e/input-group.test.js
+++ test/e2e/input-group.test.js
@@ -1,14 +1,14 @@
-const { assert, driver } = require('vl-ui-core').Test.Setup;
+const {assert, driver} = require('vl-ui-core').Test.Setup;
 const VlInputGroup = require('./pages/vl-input-group.page');
 
 describe('vl-input-group', async () => {
-    const vlInputGroup = new VlInputGroup(driver);
+  const vlInputGroup = new VlInputGroup(driver);
 
-    before(async () => {
-        return vlInputGroup.load();
-    });
+  before(async () => {
+    return vlInputGroup.load();
+  });
 
-    it("Een gebruiker kan een input-group zien", async () => {
-    	await assert.eventually.exists(vlInputGroup.getInputGroup());
-    });
+  it('Een gebruiker kan een input-group zien', async () => {
+    await assert.eventually.exists(vlInputGroup.getInputGroup());
+  });
 });
